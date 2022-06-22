02e253554b232ff551e819beb5ac36a77ed33b5c
sahayana <saokhueyang@gmail.com>
2022-06-22 14:11:21 +0900

    Build: Database setup

--- shop/settings.py
+++ shop/settings.py
@@ -92,12 +92,30 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+with open(os.path.join(BASE_DIR, "yplabs/config/mysql.json")) as f:
+    sql = json.loads(f.read())
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": sql["NAME"],
+        "USER": "root",
+        "PASSWORD": sql["PASSWORD"],
+        "HOST": "localhost",
+        "PORT": sql["PORT"],
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -122,9 +140,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
