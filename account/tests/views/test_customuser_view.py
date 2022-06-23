55a51ca7b6eb4570e14d1880fae29c5bdf1c0671
sahayana <saokhueyang@gmail.com>
2022-06-23 21:59:22 +0900

    Test: CustomUserView tested

--- account/tests/views/test_customuser_view.py
+++ account/tests/views/test_customuser_view.py
@@ -9,11 +9,11 @@ class CustomUserViewTest(APITransactionTestCase):
 
     def setUp(self) -> None:
         self.info = {
-            "username":"testuser",
-            "email":"testuser@testuser.com",
+            "username":"testuser2",
+            "email":"testuser2@testuser.com",
             "phone":"01056103812",
             "fullname":"테스터",
-            "password":"testuser@"
+            "password":"testuser2@"
         }
         
         self.client = APIClient()
@@ -22,18 +22,18 @@ class CustomUserViewTest(APITransactionTestCase):
     def test_user_can_create_single_user(self):
         
         # Given
-        data = self.info
+        info = self.info
 
         # When
         res = self.client.post(
             path="/api/accounts",
-            data=data
+            data=info
         )
 
         # Then
         self.assertEqual(201, res.status_code)
         self.assertEqual(
-            data["username"],
+            info["username"],
             CustomUser.objects.first().username            
             )
         self.assertEqual(1, CustomUser.objects.count())
@@ -42,16 +42,16 @@ class CustomUserViewTest(APITransactionTestCase):
     def test_user_never_create_user_with_invalid_info(self):
 
         # Given
-        data = self.info
+        info = self.info
 
         # Case 1: invalid email
         invalid_email = "testuser#test.com"
-        data["email"] = invalid_email
+        info["email"] = invalid_email
 
         # When
         res = self.client.post(
             path="/api/accounts",
-            data=data
+            data=info
         )
 
         # Then
@@ -61,14 +61,14 @@ class CustomUserViewTest(APITransactionTestCase):
 
 
         # Case 2: invalid password
-        data["email"] = "testuser@test.com"
+        info["email"] = "testuser@test.com"
         invalid_password = "password4"
-        data["password"] = invalid_password
+        info["password"] = invalid_password
 
         # When
         res = self.client.post(
             path="/api/accounts",
-            data=data
+            data=info
         )
 
         # Then
@@ -77,17 +77,59 @@ class CustomUserViewTest(APITransactionTestCase):
         self.assertIn("error", res.data)
 
         # Case 3: invalid phone
-        data["password"] = "password4@"
+        info["password"] = "password4@"
         invalid_phone = "0105984578a"
-        data["phone"] = invalid_phone
+        info["phone"] = invalid_phone
 
         # When
         res = self.client.post(
             path="/api/accounts",
-            data=data
+            data=info
         )
 
         # Then
         self.assertEqual(400, res.status_code)       
         self.assertEqual(0, CustomUser.objects.count())
         self.assertIn("error", res.data)
+    
+
+    def test_valid_user_can_obtain_token(self):
+
+        # Given
+        info = self.info
+        user = create_single_user(**info)        
+        data = {
+            "username": user.username,
+            "password": "testuser2@"
+        }
+
+        # When
+        res = self.client.post(
+            path="/api/accounts/token",
+            data=data
+        )
+
+        # Then
+        self.assertEqual(200, res.status_code)
+        self.assertIsNotNone(res.data["access"])
+
+    
+    def test_unvalid_user_never_obtain_token(self):
+
+        # Given
+        info = self.info
+        user = create_single_user(**info)        
+        data = {
+            "username": user.username,
+            "password": "unvaliduser@2"
+        }
+
+        # When
+        res = self.client.post(
+            path="/api/accounts/token",
+            data=data
+        )
+
+        # Then
+        self.assertEqual(401, res.status_code)  # Unauthorized
+        
