39d4e20c5346652d4acec4f5846da70789d9fe2e
sahayana <saokhueyang@gmail.com>
2022-06-26 18:04:08 +0900

    Feat: Product app serializers added

---
+++ product/serializer.py
@@ -0,0 +1,90 @@
+from rest_framework import serializers
+from product.models import (
+    Category,
+    Color,
+    Feature,
+    Size,
+    Image,
+    Information,
+    Detail,
+    Product,
+    ProductColor,
+    ProductColorSize
+)
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Category
+        fields = ["name"]
+
+
+class ColorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Color
+        fields = ["name"]
+
+
+class FeatrueSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Feature
+        fields = ["feature"]
+
+
+class SizeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Size
+        fields = ["name"]
+
+class InformationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Information
+        fields = "__all__"
+
+
+class DetailSerializer(serializers.ModelSerializer):
+
+    feature     = FeatrueSerializer(read_only= True)
+    information = InformationSerializer(read_only= True)
+
+    class Meta:
+        models = Detail
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    detail  = DetailSerializer(read_only= True)
+    color   = ColorSerializer(many= True, read_only= True)
+
+    class Meta:
+        models = Product
+        fields = "__all__"
+
+
+class ProductColorSerializer(serializers.ModelSerializer):
+
+    product = ProductSerializer(read_only= True)
+    color   = ColorSerializer(read_only= True)
+    image   = serializers.StringRelatedField(read_only= True)
+    size    = SizeSerializer(many= True, read_only= True)
+
+    class Meta:
+        models = ProductColor
+        fields = "__all__"
+
+
+class ProductColorSizeSerializer(serializers.ModelSerializer):
+
+    product_color = ProductColorSerializer(read_only= True)
+    size          = SizeSerializer(read_only= True)
+
+    class Meta:
+        models = ProductColorSize
+        fields = "__all__"
