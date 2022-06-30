ad9a9f53a11754305f21ac76a99c69f90a95d88d
sahayana <saokhueyang@gmail.com>
2022-06-30 21:12:05 +0900

    Feat: ProductLike model added

--- like/models.py
+++ like/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from account.models import CustomUser
+
+from product.models import ProductColor
 
 # Create your models here.
+
+
+
+class ProductLike(models.Model):
+
+    user    = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductColor, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "product"], name="unique_user_product"),
+        ]
